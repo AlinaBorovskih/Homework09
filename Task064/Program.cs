052e8de6bbcbc8e0caa3a5ff7ef48d4d3a87a3ad
Alina_Borovskih <alinaberseneva@icloud.com>
2022-05-30 00:53:58 +0300

    Add task 64

---
+++ Task064/Program.cs
@@ -0,0 +1,27 @@
+﻿void Rec(int N, int M)
+{
+
+    if (N > M)
+    {
+        return;
+    }
+    Console.Write(N + " ");
+    Rec(N + 1, M);
+}
+
+Rec(1, 5);
+Console.WriteLine();
+
+void Rec2 (int N, int M)
+{
+
+    if (N > M)
+    {
+        return;
+    }
+    Rec2(N + 1, M);
+    Console.Write(N + " ");
+}
+
+Rec2(1, 5);
+Console.WriteLine();
